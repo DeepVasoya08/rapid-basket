f0f3d1c766f41049f889b8cc6f7568552eafc1a4
papi <papi@localhost.localdomain>
2022-05-07 09:32:42 +0530

    initial commit

---
+++ static/js/cart.js
@@ -0,0 +1,59 @@
+let updateBtn = document.getElementsByClassName("update-cart");
+
+for (let i = 0; i < updateBtn.length; i++) {
+    updateBtn[i].addEventListener("click", function () {
+        const productId = this.dataset.product;
+        const action = this.dataset.action;
+
+        console.log("productId: ", productId, " ", "action: ", action);
+        console.log("user", user);
+
+        if (user == "AnonymousUser") {
+            addCookieItem(productId, action);
+        } else {
+            updateUserOrder(productId, action);
+        }
+    });
+}
+
+const addCookieItem = (productId, action) => {
+    if (action == "add") {
+        if (cart[productId] == undefined) {
+            cart[productId] = { "quantity": 1 };
+            console.log('added');
+        } else {
+            cart[productId]["quantity"] += 1;
+        }
+    }
+
+    if (action == "remove") {
+        cart[productId]["quantity"] -= 1;
+
+        if (cart[productId]['quantity'] <= 0) {
+            console.log('removed');
+            delete cart[productId];
+        }
+    }
+    // console.log('Cart: ', cart);
+    document.cookie = 'cart=' + JSON.stringify(cart) + ";domain;path=/"
+    location.reload();
+};
+
+const updateUserOrder = (productId, action) => {
+    let url = "/update_item/";
+
+    fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "X-CSRFToken": csrftoken,
+        },
+        body: JSON.stringify({ productId: productId, action: action }),
+    })
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            location.reload();
+        });
+};
---
+++ store/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from .models import *
+
+# Register your models here.
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
+admin.site.site_title = "Rapid Basket"
+admin.site.site_header = "Rapid Basket 🛒"
