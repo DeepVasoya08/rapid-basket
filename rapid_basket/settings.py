988ea5ebffffd07716bb8e520779880f56477550
DeepVasoya08 <deepvasoya08@gmail.com>
2022-05-28 21:15:42 +0530

    added tooltip, production improvement

--- rapid_basket/settings.py
+++ rapid_basket/settings.py
@@ -126,15 +126,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "rapid_basket/static")
 
 STATIC_URL = "static/"
 
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
-MEDIA_URL = "/images/"
+MEDIA_URL = "/static/images/"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "static/images")
+MEDIA_ROOT = os.path.join(BASE_DIR, "rapid_basket/static/images")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
